1dbedf7d5a2ee8c9d6974d9df30027d2f8a0253a
Derek Rhodes <physci@gmail.com>
2022-01-09 19:14:05 -0500

    considers some progress indication for waiting users.

--- remoxblock/remoxblock/static/js/src/remoxblock.js
+++ remoxblock/remoxblock/static/js/src/remoxblock.js
@@ -1,6 +1,7 @@
 /* Javascript for RemoXBlock. */
 function RemoXBlock(runtime, element) {
-
+    // const progress = new Progress();
+    
     function updateCount(result) {
         $('.count', element).text(result.count);
     }
@@ -11,7 +12,7 @@ function RemoXBlock(runtime, element) {
         console.log(["updating data", result]);
         $('#user-lab-data', element).text(result.result);
         $('#user-id', element).text(result.user_id);
-        $('#location', element).text(result.location);
+        $('#location', element).text(">" + result.location);
     }
     
     var handlerLoadHubData = runtime.handlerUrl(element, 'load_hub_data');
@@ -33,4 +34,47 @@ function RemoXBlock(runtime, element) {
     $(function ($) {
         /* Here's where you'd do things on page load. */
     });
+
+
+    // ------------------------------------------------------------------
+    // ux
+    // make a progress evident, 
+    // class Progress {
+    //     constructor() {
+    //         this.clocks = ["🕐", "🕑", "🕒", "🕓", "🕔", "🕕",
+    //                        "🕖", "🕗", "🕘", "🕙", "🕚", "🕛"];
+    //         this.clk_idx = 0;
+    //         this.waiting = true;            
+    //     }
+
+    //     start_timer() {
+    //         let update = _ => {
+    //             if (this.waiting) {
+    //                 this.update_display();
+    //                 this.next_clock();
+    //                 setTimeout(update, 100);
+    //             }
+    //         };
+    
+    //     }
+
+    //     next_clock() {
+    //         this.clk_idx = (this.clk_idx + 1) % this.clocks.length;
+    //     }
+    
+    //     update_display() {
+    //         if (this.is_waiting()) {
+    //             let c = this.clocks[this.clk_idx];            
+    //             $('#user-lab-data', element).text(c);
+    //             $('#user-id', element).text(c);
+    //             $('#location', element).text(c);
+    //         }
+    //     }
+
+    //     set_waiting() { this.waiting = true; }
+    //     set_not_waiting() { this.waiting = false; }
+    //     is_waiting() { return this.waiting; }
+
+
+    // }
 }
