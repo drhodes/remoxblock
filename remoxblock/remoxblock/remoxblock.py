87666148d04cdd2f09c99a4c82a3620b94323d74
Derek Rhodes <physci@gmail.com>
2022-01-12 11:28:49 -0500

    cleanup

--- remoxblock/remoxblock/remoxblock.py
+++ remoxblock/remoxblock/remoxblock.py
@@ -3,20 +3,21 @@ This xblock is for fetching remote student answers grading them
 locally at edx.
 """
 
-import pkg_resources
-import requests
-import logging
+from collections import namedtuple
 import hashlib
 import json
-from collections import namedtuple
+import logging
+import math
+import pkg_resources
+import requests
 
-from xblock.core import XBlock
-from xblock.fields import Integer, Boolean, String, Scope
-from web_fragments.fragment import Fragment
-from mako.template import Template
 from django.http import HttpResponse
-from xblockutils.studio_editable import StudioEditableXBlockMixin
+from mako.template import Template
+from web_fragments.fragment import Fragment
+from xblock.core import XBlock
+from xblock.fields import Float, Boolean, String, Scope
 from xblock.scorable import ScorableXBlockMixin, Score
+from xblockutils.studio_editable import StudioEditableXBlockMixin
 
 import remoxblock.util as util
 
@@ -32,6 +33,9 @@ class RemoXBlock(XBlock, StudioEditableXBlockMixin, ScorableXBlockMixin):
     """
     Grade Remote Json
     """
+    learner_score = Float(scope=Scope.user_state)
+
+    # configuration fields -------------------------------------------------------
     
     consumer = String(display_name="Consumer Id", help="consumer id",
                       default="", scope=Scope.settings)
@@ -60,70 +64,62 @@ class RemoXBlock(XBlock, StudioEditableXBlockMixin, ScorableXBlockMixin):
                        'secret',
                        'staff_answers')
     
-    def resource_string(self, path):
-        """Handy helper for getting resources from our kit."""
-        data = pkg_resources.resource_string(__name__, path)
-        return data.decode("utf8")
 
-    # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
         """
         The primary view of the RemoXBlock, shown to students
         when viewing courses.
         """
+        # TODO, if a learner_score is when this method is called then
+        # render those.
+
+        # TODO, if this is a rescore then also include the previous
+        # answers
+
+        # also store the previous set of answer from the lab.
+        
         html = self.resource_string("static/html/remoxblock.html")
         frag = Fragment(html.format(self=self))
         frag.add_css(self.resource_string("static/css/remoxblock.css"))
         frag.add_javascript(self.resource_string("static/js/src/remoxblock.js"))
         frag.initialize_js('RemoXBlock')
         return frag    
-    
-    def max_score(self): return 1        
 
-    def do_grade(self, answers):
-        # stub code, just getting something working.
-        max_grade = 1
-        got_grade = .5
-        #self.set_score(got_grade)
-        self.runtime.publish(self, "grade", {
-            "value": got_grade,
-            "max_value": max_grade,
-        })
-
+    def parsed_staff_answers(self):
+        # TODO consider building magic for staff to emit properly json        
+        normalized_json = self.staff_answers.replace("'", '"')
+        return json.loads(normalized_json)
+        
     def check_answer(self, lab_variable_name, lab_variable_value):
         """Arguments 
 
-        - lab_variable_name (string): student supplied variable name
-          from the lab that identifies which value is being
-          graded. The field: self.staff_answers (json map) should have
-          a key that matches lab_variable_name.
+        lab_variable_name (string): 
+
+          student supplied variable name from the lab that identifies
+          which value is being graded. The field: self.staff_answers
+          (json map) should have a key that matches lab_variable_name.
         
-        - lab_variable_value (any): is the value which is being
-          graded. The field: self.staff_answers contains this value
-          keyed by lab_variable_name.
-        
+        lab_variable_value (number): 
+
+          is the value which is being graded. The field:
+          self.staff_answers contains this value keyed by
+          lab_variable_name.
         
         Returns 
 
-        - bool: True if lab_variable_value matches what staff has
-        specified as the correct answer in staff_answers.
-
+          bool: True if lab_variable_value matches what staff has
+                specified as the correct answer in staff_answers.
         """
         
-        # TODO: work tolerance into this
+        student_answer = lab_variable_value        
         # TODO, this may throw an exception, need to handle it.
-        
-        student_answer = lab_variable_value
-
-        # TODO consider building magic for staff to emit properly json
-        normalized_json = self.staff_answers.replace("'", '"')
-        
-        staff_answers = json.loads(normalized_json)
+        staff_answers = self.parsed_staff_answers()        
         staff_answer = staff_answers[lab_variable_name]
         
         # TODO, this needs to be more robust, probably using parts of
         # an already establish grader
-        return student_answer == staff_answer
+        # TODO: work tolerance into this
+        return math.isclose(student_answer, staff_answer)
 
     def render_answers(self, answers):
         template_html = self.resource_string("static/html/answers.html")
@@ -133,7 +129,7 @@ class RemoXBlock(XBlock, StudioEditableXBlockMixin, ScorableXBlockMixin):
         for key, val in answers:
             ans = "right" if self.check_answer(key, val) else "wrong"
             rows.append(Row(key, val, ans))
-
+            
         return Template(template_html).render(rows=rows)
     
     def text_score(self, answer_pairs):
@@ -160,25 +156,25 @@ class RemoXBlock(XBlock, StudioEditableXBlockMixin, ScorableXBlockMixin):
             data={
                 "userid": anon_id,
                 "answerpath": self.answer_path,
-            },                           
+            },
             auth=(self.consumer, self.secret)
         )
-
+        
         try:
             # rsp should contain the answers from .json file.
             # by comparing the key/vals between self.answers and rsp.results
             rsp = sess.send(req.prepare())
 
             # TODO more error handling with better messages.
-            # self.do_grade("todo: json.loads that rsp.text")
-            # self._publish_grade(Score(.7, 1))
-
             answer_pairs = json.loads(rsp.text).items()
-            # log.error(answer_pairs)
 
             html = self.render_answers(answer_pairs)
             score = self.text_score(answer_pairs)
 
+            #TODO replace the hard coded 1 here with something real.
+            self.set_score(Score(1, self.max_raw_score()))
+            self.rescore(False)
+            
             return {
                 "ok": True,
                 "result": rsp.text,
@@ -188,37 +184,41 @@ class RemoXBlock(XBlock, StudioEditableXBlockMixin, ScorableXBlockMixin):
             }
         except Exception as err:
             # maybe it makes sense to drill down further into the
-            # exception, but .. users will be reporting this error, if
-            # it happens, maybe .. a remote log server would be
-            # useful?
+            # exception, but .. users will be reporting this error
+            
             msg = "Having trouble grading: " + str(err)
             log.error(msg)            
             return { "ok": False, "error": msg}
-            
-    # ------------------------------------------------------------------
-    # implementing ScoreableXBlockMixin, no idea what state is needed
-    # nor where.
 
-    # interface ScorableXBlockMixin
-    #     get_score(self)
-    #     set_score(self, score)
-    #     calculate_score(self, score)
-    #     get_score(self)
+        
+    def max_raw_score(self):
+        # this could return zero, which could lead to div by zero.
+        return len(self.parsed_staff_answers())
+
+    # ------------------------------------------------------------------
+    # implementing ScorableXBlockMixin
     
     def has_submitted_answer(self):
-        # ? how to determine this?
-        return True
+        return self.learner_score != None
     
     def get_score(self):
-        # just a stub for now.
-        return Score(raw_earned=.5, raw_possible=1)
+        if self.learner_score:
+            return Score(self.learner_score, self.max_raw_score())
+        else:
+            return Score(0, self.max_raw_score())
     
     def set_score(self, score):
-        # no idea how this is supposed to be implemented
-        pass
-    
-    def calculate_score(self, score):
-        # no idea how this is supposed to be implemented
-        return Score(raw_earned=.5, raw_possible=1)
+        #Score = namedtuple('Score', ['raw_earned', 'raw_possible'])
+        self.learner_score = score.raw_earned
+        self.save()
+        
+    def calculate_score(self):
+        return Score(self.learner_score, self.max_raw_score())
 
     ## end of ScorableXBlockMixin
+
+    
+    def resource_string(self, path):
+        """Handy helper for getting resources from our kit."""
+        data = pkg_resources.resource_string(__name__, path)
+        return data.decode("utf8")
