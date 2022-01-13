95f769839eb5e0bd1b5f0ba1ce67a7970fcf2e5e
Derek Rhodes <physci@gmail.com>
2022-01-13 18:44:01 -0500

    adds some temporary debugging code for the xblock

--- remoxblock/remoxblock/static/js/src/remoxblock.js
+++ remoxblock/remoxblock/static/js/src/remoxblock.js
@@ -9,7 +9,8 @@ function RemoXBlock(runtime, element) {
     
     function renderAnswers(html) { $('#user-lab-data', element).html(html); }
     function renderScore(score) { $('#remoxblock-score', element).text(score); }
-    function renderError(result) { $('#remoxblock-error', element).text(result); }
+    function renderError(msg) { $('#remoxblock-error', element).text(msg); }
+    function renderLearnerScore(score) { $('#remoxblock-learner-score', element).text(score); }
     function clearError() { $('#remoxblock-error', element).text(""); }
     
     function updateData(result) {
@@ -17,6 +18,7 @@ function RemoXBlock(runtime, element) {
         if (result.ok) {
             renderAnswers(result.html);
             renderScore(result.score);
+            renderLearnerScore(result.learner_score);
             clearError();
         } else {
             renderError(result.error);
@@ -37,6 +39,24 @@ function RemoXBlock(runtime, element) {
             error: function(XMLHttpRequest, textStatus, errorThrown) { 
                 unwaitCursor();
                 renderError(`Status: ${textStatus}, Error: ${errorThrown}`);
+            }});
+    });
+
+
+    var handlerResetData = runtime.handlerUrl(element, 'reset_data');
+    $('#remox-reset', element).click(function(eventObject) {
+        console.log("running LTI request");
+        waitCursor();        
+        $.ajax({
+            type: "POST",
+            url: handlerLoadHubData,
+            data: JSON.stringify({}),
+            success: function() {
+                renderAnswers("");
+                unwaitCursor();
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                unwaitCursor();
             }});
     });
     
