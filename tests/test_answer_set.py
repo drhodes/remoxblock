adcc977b9e8aeb9ecea5d1a76492e2d024fe26c4
Derek Rhodes <physci@gmail.com>
2022-01-17 13:18:04 -0500

    improves answer_set tests

--- tests/test_answer_set.py
+++ tests/test_answer_set.py
@@ -8,18 +8,21 @@ staff_json = '{"q1": 2, "q2": 4}'
 student_set = AnswerSet(student_json)
 staff_set = AnswerSet(staff_json)
 
-def test_init():
-    student_set = AnswerSet(student_json)
-    staff_set = AnswerSet(staff_json)
-    
 def test_size():
-    student_set = AnswerSet(student_json)
     assert student_set.size() == 2
-
     
 def test_share_key_val():
-    student_set = AnswerSet(student_json)
-    staff_set = AnswerSet(staff_json)
-    
     assert staff_set.shares_val(student_set, "q1")
     assert staff_set.shares_val(student_set, "q2")
+
+def test_keyvals():
+    assert (list(sorted(staff_set.keyvals()))) == [("q1", 2), ("q2", 4)]
+
+def test_num_shared_values():
+    assert staff_set.num_shared_values(student_set) == 2
+
+def test_shares_val():
+    student_set.shares_val(staff_set, "q1")
+
+def test_val():
+    assert student_set.val("q1") == 2
